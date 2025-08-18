170ae27419779e713fc4d133ab0e91f920ff5ced
ztio <ztio.top@gmail.com>
2025-08-18 15:27:54 +0800

    86. 分隔链表

---
+++ java-template/src/main/java/leetcode/editor/cn/PartitionList.java
@@ -0,0 +1,47 @@
+package leetcode.editor.cn;
+
+import leetcode.editor.common.ListNode;
+
+public class PartitionList {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode partition(ListNode head, int x) {
+            ListNode dummy1 = new ListNode(0), p1 = dummy1;
+            ListNode dummy2 = new ListNode(0), p2 = dummy2;
+            ListNode p = head;
+            while (p != null) {
+                if (p.val < x) {
+                    p1.next = p;
+                    p1 = p1.next;
+                } else {
+                    p2.next = p;
+                    p2 = p2.next;
+                }
+                ListNode temp = p.next;
+                p.next = null;
+                p = temp;
+            }
+            p1.next = dummy2.next;
+            return dummy1.next;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new PartitionList().new Solution();
+        // put your test code here
+        
+    }
+}
